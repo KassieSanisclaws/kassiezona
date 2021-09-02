ee433c348a22be3e6f5213451ee4fe5d676b1241
Kassie Sanisclaws <bytecbdeveloping@gmail.com>
2021-09-02 13:04:10 -0400

    Fourth Commit

--- src/Components/Layouts/Footer/footer.js
+++ src/Components/Layouts/Footer/footer.js
@@ -3,9 +3,56 @@ import "./footer.css";
 export default function footer() {
     return (
         <div>
-            <div className="footer-container">
-                     <div className="footer-content">
+            <div className="footer-body">
+                     <div className="footer-container">
                          <h1 className="footer-h1">Footer is working</h1>
+                           
+                           <div className="footer-content">
+                                 <div className="footer-banner">
+                                     <h3> Stay Connected With The Community!:</h3>
+                                   <li className="icon1">icon1</li>
+                                   <li className="icon2">icon2</li>
+                                   <li className="icon3">icon3</li>
+                                  </div>
+
+
+                               <div className="footer-content-container">
+                                   <div className="footer-content-container-card1">
+                                       <h2>About Us:KassieZona</h2>
+                                       <ul>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                       </ul>
+                                   </div>
+                                   <div className="footer-content-container-card2">
+                                       <h2>Products</h2>
+                                       <ul>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                       </ul>
+                                   </div>
+                                   <div className="footer-content-container-card3">
+                                       <h2>Contact</h2>
+                                       <ul>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                           <li><a href="/">Home</a></li>
+                                       </ul>
+                                   </div>
+                               </div>
+                           <div className="footer-closing-content">
+                               <h3> ©2020CopyRight: ByteCodeBlack</h3>
+                           </div>
+
+                           </div>
                     </div>
             </div>
         </div>
--- src/Components/Layouts/Home/home.js
+++ src/Components/Layouts/Home/home.js
@@ -5,6 +5,7 @@ import TopBar from "../Topbar/topbar";
 import { LoremIpsum } from "react-lorem-ipsum";
 
 
+
 export default function Home() {
     return (
         <div className="home-container">
@@ -15,19 +16,21 @@ export default function Home() {
 
                                       <div className="home-section1">
                                             <div className="home-section1-images-container">
-                                                <div className="home-section1-images-card">
-                                                    <h2> this is image1 card</h2>
+                                               
+                                                <div className="home-section1-images-card1">
+                                                     <h2> this is image2 card</h2>
                                                 </div>
-                                                <div className="home-section1-images-card">
+                                            
+                                                <div className="home-section1-images-card2">
                                                     <h2> this is image2 card</h2>
                                                 </div>
-                                                <div className="home-section1-images-card">
+                                                <div className="home-section1-images-card3">
                                                     <h2> this is image3 card</h2>
                                                 </div>
-                                                <div className="home-section1-images-card">
+                                                <div className="home-section1-images-card4">
                                                     <h2> this is image4 card</h2>
                                                 </div>
-                                                <div className="home-section1-images-card">
+                                                <div className="home-section1-images-card5">
                                                     <h2> this is image5 card</h2>
                                                 </div>
                                             </div>
@@ -42,8 +45,8 @@ export default function Home() {
                                             <ul>
                                                 <li className="lorem1"><LoremIpsum p={1} /></li>
                                                 <li className="lorem2"><LoremIpsum p={1} /></li>
-                                                <li><LoremIpsum p={1} /></li>
-                                                <li><LoremIpsum p={1} /></li>
+                                                <li className="lorem3"><LoremIpsum p={1} /></li>
+                                                <li className="lorem4"><LoremIpsum p={1} /></li>
                                                 </ul>
                                           </div>
                                       </div>
@@ -54,25 +57,30 @@ export default function Home() {
                                                   <h2>section working 3</h2>
                                              </div>
                                           <div className="home-section3-container">
-                                              <LoremIpsum p={4} />
+                                              <ul>
+                                                  <li className="lorem5"><LoremIpsum p={1} /></li>
+                                                  <li className="lorem6"><LoremIpsum p={1} /></li>
+                                                  <li className="lorem7"><LoremIpsum p={1} /></li>
+                                                  <li className="lorem8"><LoremIpsum p={1} /></li>
+                                              </ul>
                                           </div>
                                        </div>
 
                                 <div className="home-section4">
                                        <div className="home-section1-images-container">
-                                                <div className="home-section1-images-card">
+                                                <div className="home-section1-images-card6">
                                                     <h2> this is image1 card</h2>
                                                 </div>
-                                                <div className="home-section1-images-card">
+                                                <div className="home-section1-images-card7">
                                                     <h2> this is image2 card</h2>
                                                 </div>
-                                                <div className="home-section1-images-card">
+                                                <div className="home-section1-images-card8">
                                                     <h2> this is image3 card</h2>
                                                 </div>
-                                                <div className="home-section1-images-card">
+                                                <div className="home-section1-images-card9">
                                                     <h2> this is image4 card</h2>
                                                 </div>
-                                                <div className="home-section1-images-card">
+                                                <div className="home-section1-images-card10">
                                                     <h2> this is image5 card</h2>
                                                 </div>
                                             </div>
